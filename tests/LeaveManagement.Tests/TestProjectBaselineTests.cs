cde6d335115c492dabb4808cb850cead4ad82cc3
Souvik Kundu <souvikvip@outlook.com>
2026-09-19 14:07:54 +0530

    initial commit with files

---
+++ tests/LeaveManagement.Tests/TestProjectBaselineTests.cs
@@ -0,0 +1,10 @@
+namespace LeaveManagement.Tests;
+
+public sealed class TestProjectBaselineTests
+{
+    [Fact]
+    public void TestProject_is_configured()
+    {
+        Assert.True(true);
+    }
+}
